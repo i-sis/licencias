ec78e926ecda8725a5bd03b904663b16c5eb1e94
mbrachetta <mbrachetta@gmail.com>
2014-03-28 12:54:57 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -1,10 +1,13 @@
 package isis.licencias.service;
 
+import javax.validation.constraints.Pattern;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.plugins.validation.hibernate.ValidateRequest;
 
 
 @Path ("/")
@@ -12,7 +15,10 @@ public class GestorLicencias_service {
 	
 	@POST
     @Produces("application/octet-stream")
-    public Response getLicencia(@FormParam("CN") String CN,
+	@ValidateRequest
+    public Response getLicencia(@FormParam("CN") 
+    							@Pattern(regexp = "[A-Za-z ]*", message = "debe contener sólo letritas y espacios") 
+    						    String CN,
 			  					@FormParam("dni") String dni,
 			  					@FormParam("title") String title, 
 			  					@FormParam("OU") String OU,

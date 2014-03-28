b77d3c4103aff113d3a4499e8f2ee91eb53c4a9f
mbrachetta <mbrachetta@gmail.com>
2014-03-28 12:56:04 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -15,7 +15,6 @@ public class GestorLicencias_service {
 	
 	@POST
     @Produces("application/octet-stream")
-	@ValidateRequest
     public Response getLicencia(@FormParam("CN") 
     							@Pattern(regexp = "[A-Za-z ]*", message = "debe contener sólo letritas y espacios") 
     						    String CN,

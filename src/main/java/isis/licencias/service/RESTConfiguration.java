d42ddbdb49ef7f6a1f901c25741cb49487a0dbc6
mbrachetta <mbrachetta@gmail.com>
2014-03-28 12:47:15 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/RESTConfiguration.java
+++ src/main/java/isis/licencias/service/RESTConfiguration.java
@@ -1,9 +1,8 @@
 package isis.licencias.service;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("licencia")
+
 public class RESTConfiguration extends Application {
 
 }

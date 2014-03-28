9653bb91468587cf76c8f6039917903245322f35
mbrachetta <mbrachetta@gmail.com>
2014-03-28 12:17:17 -0300

    Commit from JBoss Tools

---
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -0,0 +1,30 @@
+package isis.licencias.service;
+
+import javax.validation.constraints.Pattern;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path ("/")
+public class GestorLicencias_service {
+	
+	@POST
+    @Produces("application/octet-stream")
+    public Response getLicencia(@FormParam("CN") 
+    							@Pattern(regexp = "[A-Za-z ]*", message = "debe contener sólo letritas y espacios")
+    							String CN,
+			  					@FormParam("dni") String dni,
+			  					@FormParam("title") String title, 
+			  					@FormParam("OU") String OU,
+			  					@FormParam("O") String O,
+			  					@FormParam("email") String email,
+			  					@FormParam("ST") String ST,
+			  					@FormParam("C") String C)    {
+
+		Response response = Response.ok("HOLA NUEVO MUNDO").build();
+        return response;
+	}
+}
---
+++ src/main/java/isis/licencias/service/RESTConfiguration.java
@@ -0,0 +1,9 @@
+package isis.licencias.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("licencias")
+public class RESTConfiguration extends Application {
+
+}

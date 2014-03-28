69ebde5676de560ff5673272e815bb313d02cfce
mbrachetta <mbrachetta@gmail.com>
2014-03-28 13:15:10 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.jboss.resteasy.plugins.validation.hibernate.ValidateRequest;
+
 
 
 @Path ("/")
@@ -13,6 +15,7 @@ public class GestorLicencias_service {
 	
 	@POST
     @Produces("application/octet-stream")
+	@ValidateRequest
     public Response getLicencia(@FormParam("CN") String CN,
 			  					@FormParam("dni") String dni,
 			  					@FormParam("title") String title, 

bc6af19a0cc25102a6120f7f762e6e67578a8ad3
mbrachetta <mbrachetta@gmail.com>
2014-05-13 19:34:07 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -111,7 +111,7 @@ public class GestorLicencias_service {
 			usuario.createUsuario(newUsuario);
 			
 			/* Creo archivo temporal con la licencia */ 
-			File licencia_file = crearLicencia("Firmador Digital");
+			File licencia_file = crearLicencia("Firmador Digital v2.0 - DEMO");
 			response = Response.ok((Object) licencia_file);
 			
 			/* Devuelvo un arreglo de bytes con el contenido del archivo Licencia al cliente */ 
@@ -208,7 +208,7 @@ public class GestorLicencias_service {
 			usuario.createUsuario(newUsuario);
 			
 			/* Creo archivo temporal con la licencia */ 
-			File licencia_file = crearLicencia("Firmador Digital");
+			File licencia_file = crearLicencia("Firmador Digital v2.0 - FULL");
 			response = Response.ok((Object) licencia_file);
 			
 			/* Devuelvo un arreglo de bytes con el contenido del archivo Licencia al cliente */ 

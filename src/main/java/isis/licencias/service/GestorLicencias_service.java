947bc7dce341cf7427a3468f3447ab7a530e7dea
mbrachetta <mbrachetta@gmail.com>
2015-06-17 18:39:46 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -329,7 +329,7 @@ public class GestorLicencias_service {
 		   
 	public LicenseContent createLicenseContent() {
 		 LicenseContent result = new LicenseContent();
-		 X500Principal holder = new X500Principal("CN=" + newUsuario.getName() + ", UID="+newUsuario.getDni() + ", EMAILADDRESS="+newUsuario.getEmail());
+		 X500Principal holder = new X500Principal("CN=" + newUsuario.getName() + ", UID="+newUsuario.getDni());
 		 result.setHolder(holder);
 		 X500Principal issuer = new X500Principal("CN=ISIS Consultores, L=Mendoza, O=ISIS Consultores,"
 		         								+" C=Argentina,"

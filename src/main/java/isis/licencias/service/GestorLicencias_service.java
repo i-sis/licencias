be04c67953dbf9c9323ba2d54d28fa67fd135c8f
mbrachetta <mbrachetta@gmail.com>
2016-06-14 19:03:56 -0300

    Commit from JBoss Tools

--- src/main/java/isis/licencias/service/GestorLicencias_service.java
+++ src/main/java/isis/licencias/service/GestorLicencias_service.java
@@ -214,8 +214,8 @@ public class GestorLicencias_service {
 			newUsuario.setCountry(C);
 			
 			/* Tipos Licencia 2-Base_anual, 3-Base_perpetu, 4-Full_anual, 5-Full_perpetua */
-			this.tipo_licencia = Integer.parseInt(tipo_lic);
-			newUsuario.setTipo_Licencia(new Integer(2));
+			this.tipo_licencia = Integer.parseInt("2");
+			newUsuario.setTipo_Licencia(tipo_licencia);
 			newUsuario.setFecha(new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
 			
 			usuario.createUsuario(newUsuario);
